4fb7c097a97fd530b7a9bc16b0e4e8cdd41305f5
Johnson <j3.soon@msa.hinet.net>
2022-02-08 21:24:34 +0800

    Change License from MIT to Apache

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "."},
