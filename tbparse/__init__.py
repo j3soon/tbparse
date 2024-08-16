b1a961eb7425458c546ec207b61705cee4c7b148
Johnson Sun <j3.soon777@gmail.com>
2024-08-16 12:16:58 +0800

    Add `__version__` variable

--- tbparse/__init__.py
+++ tbparse/__init__.py
@@ -4,5 +4,8 @@ summaries in a directory contains multiple event files, or a single event file.
 """
 
 from tbparse.summary_reader import SummaryReader
+from tbparse import version as _version
 
-__all__ = ['SummaryReader', ]
+__all__ = ['SummaryReader', '__version__', ]
+
+__version__ = _version.VERSION
---
+++ tbparse/version.py
@@ -0,0 +1,6 @@
+"""Contains the version string."""
+
+VERSION = "0.0.8"
+
+if __name__ == "__main__":
+    print(VERSION)
